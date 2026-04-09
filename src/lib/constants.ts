6646c52f502e7cb49228466f480a9eef15d1fa43
Thunder-Blaze <darkthunderstormshiva@gmail.com>
2026-04-09 19:11:42 +0530

    feat: Updated Constants

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -7,16 +7,15 @@ export const MAX_PAGES_TO_FETCH = 3;
 // Author associations to exclude from the leaderboard scoring
 export const IGNORED_AUTHOR_ASSOCIATIONS = ['OWNER', 'COLLABORATOR'];
 
-export const HOMEPAGE_URL =
-	'https://github.com/iiitl/FOSS-Weekend-2025?tab=readme-ov-file#welcome-to-foss-weekend-2025';
+export const HOMEPAGE_URL = 'https://github.com/iiitl/FOSS-Weekend-2026';
 export const REFRESH_INTERVAL_MS = 60000; // Client auto-refresh interval (1 min)
 
 export const EXTERNAL_CACHE_DURATION_SECONDS = 3600; // 1 hour
 
 // Event time range in IST (UTC+5:30)
 // Example: '2025-01-24T18:00:00+05:30'
-export const START_TIME_IST = '2025-01-24T18:00:00+05:30';
-export const END_TIME_IST = '2026-05-27T08:00:00+05:30';
+export const START_TIME_IST = '2025-04-10T00:00:00+05:30';
+export const END_TIME_IST = '2025-04-13T00:00:00+05:30';
 
 export const TAG_COLORS = {
 	SPECIAL: 'bg-amber-200',
